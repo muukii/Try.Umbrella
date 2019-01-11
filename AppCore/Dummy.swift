268885eb5d0802385ef2b88b3349966f1ced7cd7
Muukii <muukii.app@gmail.com>
2019-01-11 22:31:06 +0900

    Exported

--- AppCore/Dummy.swift
+++ AppCore/Dummy.swift
@@ -1,16 +1,3 @@
-//
-//  Dummy.swift
-//  AppCore
-//
-//  Created by muukii on 2019/01/08.
-//  Copyright © 2019 muukii. All rights reserved.
-//
 
 @_exported import RxSwift
 @_exported import RxCocoa
-
-import Foundation
-
-class Dummy {
-  
-}
--- Umbrella/ViewController.swift
+++ Umbrella/ViewController.swift
@@ -7,15 +7,12 @@
 //
 
 import AppCore
-//import AppCore.RxCocoa
-//import AppCore.RxSwift
-//import AppCore.RxFuture
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
     Observable<Void>.just(())
     Driver<Void>.just(())
   }
